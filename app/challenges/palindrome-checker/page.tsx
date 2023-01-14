a09d5dd935b88d517b854736bdc2fd86e6e26ac8
Roy Sheppard <rsheppard83@gmail.com>
2023-01-14 17:17:11 +0000

    challenge template

---
+++ app/challenges/palindrome-checker/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { Tab } from '@headlessui/react';
+
+const PalindromeChecker = () => {
+	return (
+		<>
+			<section className='container'>
+				<h1 className='my-16 font-primary text-light font-bold text-4xl'>
+					Palindrome Checker
+				</h1>
+			</section>
+
+			<Tab.Group as='section'>
+				<Tab.List className='container font-primary font-bold space-x-2'>
+					<Tab className='bg-primary-200 ui-selected:bg-light py-5 px-8'>
+						Overview
+					</Tab>
+					<Tab className='bg-primary-200 ui-selected:bg-light py-5 px-8'>
+						Code
+					</Tab>
+					<Tab className='bg-primary-200 ui-selected:bg-light py-5 px-8'>
+						Result
+					</Tab>
+				</Tab.List>
+
+				<Tab.Panels className='bg-light min-h-screen'>
+					<div className='container'>
+						<Tab.Panel>Content 1</Tab.Panel>
+						<Tab.Panel>Content 2</Tab.Panel>
+						<Tab.Panel>Content 3</Tab.Panel>
+					</div>
+				</Tab.Panels>
+			</Tab.Group>
+		</>
+	);
+};
+
+export default PalindromeChecker;
