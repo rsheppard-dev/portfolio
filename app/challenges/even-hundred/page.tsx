694ffe8841840ca44811abdad747c26ab23d3a5f
Roy Sheppard <rsheppard83@gmail.com>
2023-01-18 10:35:13 +0000

    add iframe

--- app/challenges/even-hundred/page.tsx
+++ app/challenges/even-hundred/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import ts from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
 import { monokaiSublime } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
@@ -10,83 +9,80 @@ import Breadcrumbs from '../../../components/Breadcrumbs';
 SyntaxHighlighter.registerLanguage('typescript', ts);
 
 const EvenHundred = () => {
-	const codeString = `
-	// function to get values from user interface
-	const getValues = (e: Event): void => {
-		// prevent default form behaviour (page refresh)
-		e.preventDefault();
+	const codeString = `// function to get values from user interface
+const getValues = (e: Event): void => {
+	// prevent default form behaviour (page refresh)
+	e.preventDefault();
 
-		// get elements from html page
-		const startValueInput = document.getElementById(
-			'startValue'
-		) as HTMLInputElement;
-		const endValueInput = document.getElementById('endValue') as HTMLInputElement;
-		const errorBox = document.getElementById('error') as HTMLDivElement;
+	// get elements from html page
+	const startValueInput = document.getElementById(
+		'startValue'
+	) as HTMLInputElement;
+	const endValueInput = document.getElementById('endValue') as HTMLInputElement;
+	const errorBox = document.getElementById('error') as HTMLDivElement;
 
-		// get values from inputs and parse to integers
-		const startValue = parseInt(startValueInput.value);
-		const endValue = parseInt(endValueInput.value);
+	// get values from inputs and parse to integers
+	const startValue = parseInt(startValueInput.value);
+	const endValue = parseInt(endValueInput.value);
 
-		// check values entered by user are valid numbers
-		if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-			// call generateNumbers
-			const numbers = generateNumbers(startValue, endValue);
+	// check values entered by user are valid numbers
+	if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+		// call generateNumbers
+		const numbers = generateNumbers(startValue, endValue);
 
-			// call displayNumbers function
-			displayNumbers(numbers);
+		// call displayNumbers function
+		displayNumbers(numbers);
+	} else {
+		// display error on page if invalid input
+		errorBox.innerText = 'You can only enter numbers in the inputs above.';
+		errorBox.classList.remove('d-none');
+	}
+};
+
+// function to generate numbers between startValue and endValue
+const generateNumbers = (startValue: number, endValue: number): number[] => {
+	const numbers: number[] = [];
+
+	// loop through all numbers from startValue to endValue
+	for (let i = startValue; i <= endValue; i++) {
+		// add current number to end of numbers array
+		numbers.push(i);
+	}
+
+	return numbers;
+};
+
+// function to display number range
+const displayNumbers = (numbers: number[]): void => {
+	let templateRows = '';
+
+	// loop through all the numbers
+	for (let i = 0; i < numbers.length; i++) {
+		const number = numbers[i];
+		let className: string;
+
+		// check if current number is even or odd and assign correct class
+		if (number % 2 === 0) {
+			className = 'even';
 		} else {
-			// display error on page if invalid input
-			errorBox.innerText = 'You can only enter numbers in the inputs above.';
-			errorBox.classList.remove('d-none');
-		}
-	};
-
-	// function to generate numbers between startValue and endValue
-	const generateNumbers = (startValue: number, endValue: number): number[] => {
-		const numbers: number[] = [];
-
-		// loop through all numbers from startValue to endValue
-		for (let i = startValue; i <= endValue; i++) {
-			// add current number to end of numbers array
-			numbers.push(i);
+			className = 'odd';
 		}
 
-		return numbers;
-	};
+		// update template rows
+		templateRows += '<tr><td class="' + className + '">' + number + '<td></tr>';
+	}
 
-	// function to display number range
-	const displayNumbers = (numbers: number[]): void => {
-		let templateRows = '';
+	// update html in table body
+	document.getElementById('results')!.innerHTML = templateRows;
+};
 
-		// loop through all the numbers
-		for (let i = 0; i < numbers.length; i++) {
-			const number = numbers[i];
-			let className: string;
+// get button element
+const submitButton = document.getElementById('submit') as HTMLButtonElement;
 
-			// check if current number is even or odd and assign correct class
-			if (number % 2 === 0) {
-				className = 'even';
-			} else {
-				className = 'odd';
-			}
+// create event listener for when user clicks button
+submitButton.addEventListener('click', getValues);
 
-			// update template rows
-			templateRows += '<tr><td class="' + className + '">' + number + '<td></tr>';
-		}
-
-		// update html in table body
-		document.getElementById('results')!.innerHTML = templateRows;
-	};
-
-	// get button element
-	const submitButton = document.getElementById('submit') as HTMLButtonElement;
-
-	// create event listener for when user clicks button
-	submitButton.addEventListener('click', getValues);
-
-	export {};
-	
-`;
+export {};	`;
 	return (
 		<section className='min-h-screen flex flex-col'>
 			<h1 className='container mt-16 mb-5 font-primary text-light font-bold text-4xl'>
@@ -128,7 +124,11 @@ const EvenHundred = () => {
 								Code
 							</h2>
 
-							<SyntaxHighlighter language='typescript' style={monokaiSublime}>
+							<SyntaxHighlighter
+								language='typescript'
+								showLineNumbers={true}
+								style={monokaiSublime}
+							>
 								{codeString}
 							</SyntaxHighlighter>
 						</Tab.Panel>
@@ -139,13 +139,14 @@ const EvenHundred = () => {
 							<h2 className='font-primary font-bold text-xl text-primary-300 mb-10'>
 								Result
 							</h2>
+
 							<iframe
-								src='https://rsheppard-dev.github.io/even-hundred'
+								src='https://evenhundred.netlify.app/'
 								width='100%'
 								height='100%'
 								className='min-h-screen'
-								sandbox='allow-forms allow-scripts allow-presentation'
-							/>
+								sandbox=''
+							></iframe>
 						</Tab.Panel>
 					</div>
 				</Tab.Panels>
