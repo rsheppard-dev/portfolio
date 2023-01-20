8413dd18362700dd5ace6b045c37ce62a24ef1d6
Roy Sheppard <rsheppard83@gmail.com>
2023-01-20 20:04:00 +0000

    fix data type

--- data/projectData.ts
+++ data/projectData.ts
@@ -5,7 +5,11 @@ const projectData: IProject[] = [
 		title: 'Bug Crunch',
 		description:
 			'A full-scale enterprise web application. Used by teams to help find, track and fix bugs in their applications.',
-		image: '',
+		image: {
+			src: '#',
+			width: 100,
+			height: 100,
+		},
 		tech: ['HTML5', 'CSS3', 'JavaScript', 'C#'],
 		links: {
 			readme: '',
@@ -17,7 +21,11 @@ const projectData: IProject[] = [
 		title: 'Movie Contender',
 		description:
 			'An application that connects to a movie API and generates a quiz based on the response.',
-		image: '',
+		image: {
+			src: '#',
+			width: 100,
+			height: 100,
+		},
 		tech: ['HTML5', 'CSS3', 'TypeScript', 'C#'],
 		links: {
 			readme: '',
