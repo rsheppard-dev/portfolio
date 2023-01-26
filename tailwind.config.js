bff7936220af1b94602bced8549856a1f7d371b7
Roy Sheppard <rsheppard83@gmail.com>
2023-01-26 20:54:39 +0000

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 const { screens } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./utils/**/*.{js,ts,jsx,tsx}',
+	],
 	theme: {
 		screens: {
 			xs: '375px',
