b25205f81acd2efeae57a5662ebf8214f278f6e8
Roy Sheppard <rsheppard83@gmail.com>
2024-01-09 17:10:48 +0000

    Refactor SanityImage component to handle null value

--- components/SanityImage.tsx
+++ components/SanityImage.tsx
@@ -5,11 +5,13 @@ import { SanityAsset } from '@sanity/image-url/lib/types/types';
 import { urlFor } from '../server/sanity.client';
 
 export default function SanityImage({ value }: { value: SanityAsset }) {
-	const url = urlFor(value).fit('max').auto('format').url();
+	if (value === null) return null;
+
+	const url = urlFor(value)?.fit('max')?.auto('format')?.url();
 	return (
 		<div className=''>
 			<Image
-				src={urlFor(value).fit('max').auto('format').url()}
+				src={urlFor(value)?.fit('max')?.auto('format')?.url()}
 				alt={value?.alt}
 				width={300}
 				height={300}
